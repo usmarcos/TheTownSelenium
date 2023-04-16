65df2795e519ee1d7de1e1e429952503cf648288
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-04-16 01:07:45 -0300

    Codigos basicos para facilitar a criação da automação dos cenários

--- src/test/java/town/ada/RunCucumberTest.java
+++ src/test/java/town/ada/RunCucumberTest.java
@@ -12,4 +12,5 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("town/ada")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
+
 }
--- src/test/java/town/ada/StepDefinitions.java
+++ src/test/java/town/ada/StepDefinitions.java
@@ -1,17 +1,30 @@
 package town.ada;
 
 import io.cucumber.java.en.*;
-
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinitions {
-
     @Given("an example scenario")
     public void anExampleScenario() {
+        System.setProperty("webdriver.chrome.driver", "drive\\chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://thetown.com.br/pt/");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("INFORMAÇÕES")).click();
+        this.timeView(1000L);
+        String page = driver.getCurrentUrl();
+        Assertions.assertTrue(page.contains("/informacoes"));
+        this.timeView(1000L);
+        driver.close();
+        System.out.println("an example scenario");
     }
 
     @When("all step definitions are implemented")
     public void allStepDefinitionsAreImplemented() {
+        System.out.println("all step definitions are implemented");
     }
 
     @Then("the scenario passes")
@@ -19,4 +32,11 @@ public class StepDefinitions {
         System.out.println("Teste");
     }
 
+    private void timeView(Long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
